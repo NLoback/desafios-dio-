b6aa84fad3031bb02fbd196b05586ed36dcf10a9
NLoback <nicolasloback@gmail.com>
2022-01-24 13:36:33 -0300

    confirmar

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# desafios-dio-
+# desafios-dio- BootCamp Take-Blip_Web Inicio dos estudos
---
+++ projetos-curso/primeiros passos web php/ex01.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+    <title>Meu primeiro site em PHP!! </title>
+</head>
+
+<body>
+
+
+    <?php
+    for ($i = 0; $i < 10; $i++) {
+        print(" Linha numero" . $i . "<br>");
+    }
+    ?>
+
+
+</body>
+
+</html>
